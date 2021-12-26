8ba39e53149f6f4ae9a3dbaf65afbf33f42c7a81
Nguyen Dang Khoa <khoadangnguyen2012@gmail.com>
2021-12-27 01:39:05 +0700

    code: change inputs

--- Main.java
+++ Main.java
@@ -1,12 +1,51 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.ItemBuilder;
 import model.ItemComponent;
 import model.ItemType;
 import model.Reciept;
 import strategy.Excel;
+
 public class Main {
-    public static void main(String[] args) {
-        //Instantiate objects
+    public static List<ItemComponent> input_1() {
+        List<ItemComponent> itemList = new ArrayList<>();
+
+        ItemComponent book  = new ItemBuilder(1, "book", 12.49)
+            .setType(ItemType.BOOK)
+            .build();
+        ItemComponent musicCd  = new ItemBuilder(1, "music cd", 14.99)
+            .build();
+        ItemComponent chocolateBar  = new ItemBuilder(1, "chocolate bar", 0.85)
+            .setType(ItemType.FOOD)
+            .build();
+        
+        itemList.add(book);
+        itemList.add(musicCd);
+        itemList.add(chocolateBar);
+
+        return itemList;
+    }
+    public static List<ItemComponent> input_2() {
+        List<ItemComponent> itemList = new ArrayList<>();
+
+        ItemComponent importedChocolateBox  = new ItemBuilder(1, "imported box of chocolates", 10.00)
+            .setImported(true)
+            .setType(ItemType.FOOD)
+            .build();
+        ItemComponent importedPerfumeBottle = new ItemBuilder(1, "imported bottle of perfume", 47.50)
+            .setImported(true)
+            .build();
+            
+        itemList.add(importedChocolateBox);
+        itemList.add(importedPerfumeBottle);
+
+        return itemList;
+    }
+    public static List<ItemComponent> input_3() {
+        List<ItemComponent> itemList = new ArrayList<>();
+
         ItemComponent importedPerfumeBottle = new ItemBuilder(1, "imported bottle of perfume", 27.99)
                                 .setImported(true)
                                 .build();
@@ -19,13 +58,23 @@ public class Main {
                                 .setImported(true)
                                 .build();
         
+        itemList.add(importedPerfumeBottle);
+        itemList.add(perfumeBottle);
+        itemList.add(headachePill);
+        itemList.add(chocolateBox);
+
+        return itemList;
+    }
+    public static void main(String[] args) {
+        //Get items
+        //List<ItemComponent> items = input_1();
+        //List<ItemComponent> items = input_2();
+        List<ItemComponent> items = input_3();
+        
         //Excel excelFormatter = new Excel();
         Reciept reciept = new Reciept();
-        //reciept.setFormatter(excelFormatter);;
-        reciept.addItem(importedPerfumeBottle);
-        reciept.addItem(perfumeBottle);
-        reciept.addItem(headachePill);
-        reciept.addItem(chocolateBox);
+        //reciept.setFormatter(excelFormatter);
+        reciept.addItemList(items);
         reciept.format();
     }
 }
